5c87c1efaba28cb0d074d48dde650e707f4b17db
Evgeny <bulon.danilov1999@mail.ru>
2025-05-13 13:48:44 +0300

    Цвет для Icon нижнего меню

--- app/src/main/java/com/example/cafeapp/MenuScreenActivity.kt
+++ app/src/main/java/com/example/cafeapp/MenuScreenActivity.kt
@@ -1,20 +1,28 @@
 package com.example.cafeapp
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.cafeapp.databinding.ActivityMenuScreenBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MenuScreenActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMenuScreenBinding
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_menu_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_menu_screen)
+
+
     }
 }
